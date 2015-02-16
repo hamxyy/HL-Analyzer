e6ff9e1380abd04965726fd4bc492b43ae4fff5c
Zhu Zhong <hamxyy@Gmail.com>
2015-02-16 10:26:49 +0800

    Clean up workspace
    Remove useless folders, move dependency to source folder, check in the
    test hl project.

--- Cucumber_HAL/HearingLanguage/Source/com.shs.assembly.generator/GenerateCSharp.bat
+++
@@ -1,5 +0,0 @@
-SET eclipseDir=..\
-
-SET eclipseLauncher=%eclipseDir%\plugins\org.eclipse.equinox.launcher_1.3.0.v20130327-1440.jar
-
-java -jar %eclipseLauncher% -application org.eclipse.ant.core.antRunner -buildfile GenerateCSharp.xml -propertyfile GenerateCSharp.properties %*
